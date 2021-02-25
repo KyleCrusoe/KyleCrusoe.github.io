885f1ae079b0144af4920065b6c8fca2d8ab5b7b
KyleCrusoe <kylecrusoeburner@gmail.com>
2021-02-25 06:39:30 -0500

    Functional Multi-Image Modal. Added shake effect to thumbnails for user feedback.

--- scripts.js
+++ scripts.js
@@ -1,24 +1,8 @@
-// create references to the modal...
-var modal = document.getElementById('modal');
-// to all images -- note I'm using a class!
-var images = document.getElementsByClassName('thumbnail');
-// image in the modal
-var modalImg = document.getElementsById("cryptoImg");
-// the caption in the modal
-var captionText = document.getElementById("caption");
+var modal = document.getElementsByClassName("modal");
 
-// Go through all of the images with our custom class
-for (var i = 0; i < images.length; i++) {
-  var img = images[i];
-  img.onclick = function() {
-    modal.style.display = "block";
-    modalImg.src = ;
-    captionText.innerHTML = this.alt;
-  }
+function modalshow(number){
+    modal[number].style.display = "block";
+
 }
 
-var span = document.getElementsByClassName("close")[0];
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
+console.log(modal[0]);

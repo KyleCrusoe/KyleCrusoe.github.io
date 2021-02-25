98281239b6872e540cf8f6c8840dfef051a9fe65
KyleCrusoe <kylecrusoeburner@gmail.com>
2021-02-25 06:36:32 -0500

    More work on modals.

--- scripts3rdtry.js
+++ scripts3rdtry.js
@@ -1,15 +1,8 @@
 var modal = document.getElementsByClassName("modal");
-var image = document.getElementsByClassName("thumbnail");
 
-for (var i = 0; i < image.length; i++) {
-    var img = image[i];
-    img.onclick = function(){
-        var counter = i;
-        modal[counter].style.display = "block";
-    }
+function modalshow(number){
+    modal[number].style.display = "block";
 
 }
-
-if 
 
 console.log(modal[0]);

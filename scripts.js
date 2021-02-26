a05e6833c9327bdc6647891cfad59c2d12b9aa53
KyleCrusoe <kylecrusoeburner@gmail.com>
2021-02-26 00:46:03 -0500

    Scalable modal functionality. Add 3 forms of payment. Add textures to background. Add hash values for crypto and memos when necessary.

--- scripts.js
+++ scripts.js
@@ -2,7 +2,20 @@ var modal = document.getElementsByClassName("modal");
 
 function modalshow(number){
     modal[number].style.display = "block";
+    
+    var span = document.getElementsByClassName("modal")[number];
 
+    span.onclick = function() {
+        modal[number].style.display = "none";
 }
 
 console.log(modal[0]);
+
+
+  }
+
+//   window.onclick = function(event) {
+//     if (event.target == modal) {
+//       modal.style.display = "none";
+//     }
+//   }
